b906e82f40548e86bf21747c4cc2d2261c6f583c
picky37 <149702922+picky37@users.noreply.github.com>
2024-09-24 16:16:19 +0200

    Déclarations variables sass

--- header.php
+++ header.php
@@ -10,6 +10,7 @@
 <body <?php body_class(); ?>>
     <header class="my-header">
        <a href="accueil"><img src="wp-content\themes\Theme-Nathalie-Mota\images\logo_nathalie_mota.svg" alt="Logo de Nathalie Mota"></a>
+       <h1>Coucou les amis</h1>
     </header>
     <?php wp_nav_menu( array( 'header-menu' => 'header-menu' ) ); ?>
 </body>
