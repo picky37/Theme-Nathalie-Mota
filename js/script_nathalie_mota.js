2fbb81bba494b8ccc2c6a2e42a8a64a6ce4f18ff
picky37 <malinowski.noe@gmail.com>
2024-10-11 09:00:53 +0200

    Modif header et autres

--- js/script_nathalie_mota.js
+++ js/script_nathalie_mota.js
@@ -12,10 +12,40 @@ menuLink.addEventListener('click', () => {
     console.log('close');
 });
  
-const burger = document.querySelector('.logo_burger');
+const burger1 = document.querySelector('.logo_burger1');
+const burger2 = document.querySelector('.logo_burger2');
 const menuMobile = document.querySelector('.modal');
-console.log(menuMobile);
-burger.addEventListener('click', () => {
-    console.log('burger cliqué');
+
+burger1.addEventListener('click', () => {
+    console.log('burger 1 cliqué');
     menuMobile.classList.toggle('changeMenuMobile');
 });
+
+burger2.addEventListener('click', () => {
+    console.log('burger 2 cliqué');
+    menuMobile.classList.toggle('changeMenuMobile');
+});
+
+function chargerPage() {
+    fetch('page.html') // Remplacez par l'URL de votre page à charger
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erreur réseau');
+            }
+            return response.text();
+        })
+        .then(html => {
+            // Créer un élément temporaire pour obtenir le contenu du body
+            const tempDiv = document.createElement('div');
+            tempDiv.innerHTML = html;
+
+            // Extraire le contenu du body
+            const bodyContent = tempDiv.querySelector('body').innerHTML;
+
+            // Insérer le contenu dans la page actuelle
+            document.getElementById('contenu').innerHTML = bodyContent;
+        })
+        .catch(error => {
+            console.error('Erreur lors du chargement de la page:', error);
+        });
+}
--- templates_part/menu_mobile.php
+++ templates_part/menu_mobile.php
@@ -1,19 +1,8 @@
 <div class="modal">
-
-
-
-
     <header class="my-header-mobile">
         <a href="accueil"><img class="img_logo" src="<?= THEME_URI ?>/images/logo_nathalie_mota.svg" alt="Logo de Nathalie Mota"></a>
-        <img src="<?= THEME_URI ?>/images/Menu.svg" alt="Logo menu hamburger" class="logo_burger">
-        <?php
-        wp_nav_menu(array('wp-content\themes\Theme-Nathalie-Mota' => 'Menu 1'));
-        ?>
+        <img src="<?= THEME_URI ?>/images/Menu.svg" alt="Logo menu hamburger" class="logo_burger2">
     </header>
-
-
-
-
     <div class="navbar-mobile-list">
         <?php
         wp_nav_menu(array('wp-content\themes\Theme-Nathalie-Mota' => 'Menu 1'));
