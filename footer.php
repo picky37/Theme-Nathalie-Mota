a6737bcb0536989f59fb7ace1428bee2147e97b2
picky37 <malinowski.noe@gmail.com>
2024-10-11 11:26:18 +0200

    Finition footer

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 <footer>
-  
+<?php wp_nav_menu(array('menu' => 'Menu 2')); ?>
 </footer>
-<?php wp_footer(); ?>
+<?php wp_footer();?>
+
+
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php get_header(); ?>
 <main class="wrap">
-  <p>quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime
-
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  quo esse ratione maxime atque, illo, ducimus veniam aliquid eius sequi delectus quisquam nobis deleniti non!
-  </p>
-
+  <img class="image_header_desktop" src="/wp-content/themes/Theme-Nathalie-Mota/images/image_header_desktop.png" alt="Réunion de plusieurs personnes pour une fête">
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius eaque earum delectus, est dolorem aliquam beatae iste quibusdam, esse tempora deserunt amet ora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quoora deserunt amet voluptatibus soluta. Sed neque modi facere quo voluptatibus soluta. Sed neque modi facere quo quibusdam?</p>
   <?php include 'wp-content\themes\Theme-Nathalie-Mota\templates_part\modale_contact.php';?>
   
 
