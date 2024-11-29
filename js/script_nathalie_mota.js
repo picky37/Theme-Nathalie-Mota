423d69d788a7dbf7dc1084adc4ab12ea940dc62d
picky37 <malinowski.noe@gmail.com>
2024-11-29 15:13:23 +0100

    Nettoyage JS

--- js/script_nathalie_mota.js
+++ js/script_nathalie_mota.js
@@ -1,58 +1,54 @@
-console.log('connecté !');
-
+// Stocke la modale de contact
 const popup = document.getElementById('modale_container');
-console.log(popup);
-const closeButton = document.getElementById('closeButton');
+
+// Stocke le lien "CONTACT" du header
 const menuLink = document.getElementById('menu-item-26');
+
+// Stocke le lien "CONTACT" d'un single post
 const menuLinkSingle = document.getElementById('menu-item-x');
 
-
+// Logique d'apparition/disparition de la modale de contact depuis le header
 menuLink.addEventListener('click', () => {
     popup.classList.toggle('show');
-    console.log('close');
 });
 
+// Logique d'apparition/disparition de la modale de contact depuis un single post
 menuLinkSingle.addEventListener('click', () => {
     popup.classList.toggle('show');
-    console.log('close');
 });
- 
+
+// Stocke le logo burger du header mobile
 const burger1 = document.querySelector('.logo_burger1');
+
+// Stocke le logo burger du menu déroulé à fond rouge
 const burger2 = document.querySelector('.logo_burger2');
+
+// Stocke le menu à fond rouge de la version mobile
 const menuMobile = document.querySelector('.modal');
 
+// Logique d'apparition du menu déroulant
 burger1.addEventListener('click', () => {
-    console.log('burger 1 cliqué');
     menuMobile.classList.toggle('changeMenuMobile');
 });
 
+// Logique de disparaition du menu déroulant
 burger2.addEventListener('click', () => {
-    console.log('burger 2 cliqué');
     menuMobile.classList.toggle('changeMenuMobile');
 });
 
 //Code pour préremplir le champ "référence" du formulaire de contact 
-
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-
+document.addEventListener("DOMContentLoaded", function () {
     var referenceContainer = document.getElementById("reference-container");
     if (referenceContainer) {
         var reference = referenceContainer.getAttribute("data-reference");
-        console.log("Référence récupérée :", reference);
     }
 
     // Sélectionner le champ de formulaire avec son ID
     var inputField = document.getElementById("wpforms-15-field_7");
-    
+
     // Vérifier si l'élément existe sur la page
     if (inputField) {
         // Préremplir le champ avec une valeur numérique par défaut
         inputField.value = reference;
-        console.log(inputField.value);
     }
 });
--- template_parts/photo_block.php
+++ template_parts/photo_block.php
@@ -19,12 +19,30 @@
             <?php
             // Afficher l'image mise en avant ou le contenu si aucune mise en avant
             if (has_post_thumbnail()) {
+
+                echo '<div class="lightbox">
+                <button class="lightbox__close">Fermer</button>
+                <button class="lightbox__next">Suivant</button>
+                <button class="lightbox__prev">Précédent</button>
+                <div class="lightbox__container">';
                 the_post_thumbnail('large', ['class' => 'photo-apparentee']);
+                echo'</div>';
+              echo'</div>';
+
             } else {
-                // Afficher le contenu de l'article si aucune image mise en avant
+
+                echo '<div class="lightbox">
+                <button class="lightbox__close">Fermer</button>
+                <button class="lightbox__next">Suivant</button>
+                <button class="lightbox__prev">Précédent</button>
+                <div class="lightbox__container">';
                 echo '<div class="article-content">';
                 echo the_content(); // the_content() affiche le contenu de l'article
                 echo '</div>';
+                echo'</div>';
+              echo'</div>';
+                // Afficher le contenu de l'article si aucune image mise en avant
+                
             } ?>
 
     <?php
