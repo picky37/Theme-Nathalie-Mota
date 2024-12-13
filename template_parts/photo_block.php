2fefc61d4fb658752ce68653876f878f7e5cda71
picky37 <malinowski.noe@gmail.com>
2024-12-13 10:14:43 +0100

    Avancement lightbox

--- template_parts/photo_block.php
+++ template_parts/photo_block.php
@@ -13,39 +13,34 @@
         while ($related_query->have_posts()) : $related_query->the_post();
     ?>
 
-        <a class="accueil" class="photo-link">
-            <!-- <img src="wp-content\themes\Theme-Nathalie-Mota\images\regard.svg" alt="Logo" class="regard"> -->
+            <a class="accueil" class="photo-link">
+                <!-- <img src="wp-content\themes\Theme-Nathalie-Mota\images\regard.svg" alt="Logo" class="regard"> -->
 
-            <?php
-            // Afficher l'image mise en avant ou le contenu si aucune mise en avant
-            if (has_post_thumbnail()) {
-                the_post_thumbnail('large', ['class' => 'photo-apparentee']);
-            } else {
-                // Afficher le contenu de l'article si aucune image mise en avant
-                echo '<div class="lightbox">
-  <button class="lightbox__close">Fermer</button>
-  <button class="lightbox__next">Suivant</button>
-  <button class="lightbox__prev">Précédent</button>
-  <div class="lightbox__container">';
-    echo '<a href="' . get_permalink() . '">';
+                <?php
+                // Afficher l'image mise en avant ou le contenu si aucune mise en avant
 
-  
-  echo the_content(); // the_content() affiche le contenu de l'article
-
-echo'</a>';
- echo '</div>';
-echo '</div>';
+               
                 
-                
-            } 
-            ?>
+echo ' <a href="' . get_permalink() . '" class="lightbox" title="">';
 
-        </a>
+                echo the_content(); // the_content() affiche le contenu de l'article
+
+                echo '</a>';
+               
+                
+            
+
+
+                ?>
+
+            </a>
 
     <?php
         endwhile;
         wp_reset_postdata();
     endif;
     ?>
+
+
 
 </div>
