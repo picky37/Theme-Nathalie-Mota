979f5d36b5828429ab328c42942ef4b9a0e78f13
picky37 <malinowski.noe@gmail.com>
2025-02-12 16:44:11 +0100

    Ajout navigation inter posts

--- single.php
+++ single.php
@@ -62,19 +62,105 @@ get_header();
 
     </div>
 
-<div class="traitNoir"></div>
+
+
+
+
+
+
+    <div class="post-nav">
+        <?php
+        $prev_post = get_previous_post();
+        $next_post = get_next_post();
+        ?>
+
+        <?php if ($prev_post || $next_post) : ?>
+            <div class="post-navigation">
+                <!-- Miniature fixe au-dessus des flèches -->
+                <div class="nav-thumbnail"></div>
+
+                <?php if ($prev_post) : ?>
+                    <div class="nav-item">
+                        <a href="<?php echo get_permalink($prev_post); ?>">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/previous_arrow_single_post.svg" alt="Précédent">
+                        </a>
+                        <div class="nav-thumbnail" style="background-image: url('<?php echo get_the_post_thumbnail_url($prev_post->ID, 'medium'); ?>');"></div>
+                    </div>
+                <?php endif; ?>
+
+                <?php if ($next_post) : ?>
+                    <div class="nav-item">
+                        <a href="<?php echo get_permalink($next_post); ?>">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/next_arrow_single_post.svg" alt="Suivant">
+                        </a>
+                        <div class="nav-thumbnail next" style="background-image: url('<?php echo get_the_post_thumbnail_url($next_post->ID, 'medium'); ?>');"></div>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endif; ?>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+    <div class="traitNoir"></div>
 
     <div class="post-related">
 
         <h3>Vous aimerez aussi</h3>
 
-        <?php 
+        <?php
         $number_of_photos = 2;
         $myOrderby = "rand";
-        include 'wp-content/themes/Theme-Nathalie-Mota/template_parts/photo_block.php'; 
+        include 'wp-content/themes/Theme-Nathalie-Mota/template_parts/photo_block.php';
         ?>
     </div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <?php
 get_footer();
